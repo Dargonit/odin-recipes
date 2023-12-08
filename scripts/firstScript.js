54029fb6bf6727b99317de7f7d850a003e30e8f0
Dargonit <igrokyse@gmail.com>
2023-12-08 14:45:31 -0800

    added exersice functions to first script: -add7() -multiply -capitalize -lastLetter

--- scripts/firstScript.js
+++ scripts/firstScript.js
@@ -1,1 +1,18 @@
 console.log("Hello world");
+
+function add7(number){
+    return number+7;
+}
+
+function multiply(numOne, numTwo){
+    return numOne * numTwo;
+}
+
+function capitalize(str){
+    const firstLetter = str.charAt(0).toUpperCase();
+    return  firstLetter + str.slice(1).toLowerCase();
+}
+
+function lastLetter(str){
+    return str.slice(-1);
+}
